da77d65787e2defb414e2c94bcf5bbb27d78523d
PieThon13 <74686038+PieThon13@users.noreply.github.com>
2020-11-26 19:30:20 +0100

    Add files via upload

---
+++ Rzut_w_stałym_polu_grawitacyjnym.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+h0=float(input('Z jakiej wysokości wyrzucono ciało?'))
+v0=float(input('Jaka jest wartość prędkości początkowej?'))
+a0=float(input('Ile wynosi wartość przyśpieszenia grawitacyjnego?'))
+theta=float(input('Pod jakim kątem wyrzucono ciało?'))
