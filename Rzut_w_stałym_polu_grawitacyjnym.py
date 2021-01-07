137ab490626a1a791b614ed0c7af8e45beb6e579
PieThon13 <mikusia154@wp.pl>
2021-01-07 22:03:39 +0100

    Ostatni commit

--- Rzut_w_stałym_polu_grawitacyjnym.py
+++ Rzut_w_stałym_polu_grawitacyjnym.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from vpython import *
 def rzut1(h0,v0,a,theta):
     H=h0
     tc=(v0*np.sin((360-theta)*np.pi/180)-np.sqrt((v0**2*np.sin((360-theta)*np.pi/180)**2)+2*a*h0))/(-a)
@@ -42,6 +43,31 @@ def rzut1(h0,v0,a,theta):
         plt.grid(True)
         plt.savefig('WYKRES.jpg')
         plt.show
+
+        def Reset(q):
+            ball.make_trail = False
+            ball.clear_trail()
+            time = 0
+            ball.pos = vector(0, h0, 0)
+            ball.make_trail = True
+            while time <= tc:
+                sleep(0.05)
+                ball.pos.x = vx * time
+                ball.pos.y = h0 - v0 * np.sin((360 - theta) * np.pi / 180) * time - 0.5 * a * time ** 2
+                time = time + dt
+
+        scene = canvas(title='Symulacja rzutu', autoscale=True)
+        button(bind=Reset, pos=scene.caption_anchor, text='Reset')
+        ball = sphere(pos=vector(0, h0, 0), radius=1, color=color.magenta, make_trail=True, trail_color=color.white,
+                      trail_radius=0.1)
+        ground = box(pos=vector(0, 0, 0), length=3 * z, height=0.01, width=100, opacity=0.5, color=color.orange)
+        time = 0
+        dt = 0.05
+        while time <= tc:
+            sleep(0.05)
+            ball.pos.x = vx * time
+            ball.pos.y = h0 - v0 * np.sin((360 - theta) * np.pi / 180) * time - 0.5 * a * time ** 2
+            time = time + dt
     else:
         fig, ax = plt.subplots(2, 1)
         fig.tight_layout(h_pad=4)
@@ -63,6 +89,36 @@ def rzut1(h0,v0,a,theta):
         plt.grid(True)
         plt.savefig('WYKRES.jpg')
         plt.show()
+
+        def Reset(q):
+            ball.make_trail = False
+            ball.clear_trail()
+            time = 0
+            ball.pos = vector(0, h0, 0)
+            ball.make_trail = True
+            while time <= tc:
+                sleep(0.05)
+                ball.pos.y = h0 - v0 * np.sin((360 - theta) * np.pi / 180) * time - 0.5 * a * time ** 2
+                time = time + dt
+
+        scene = canvas(title='Symulacja rzutu', autoscale=True)
+        button(bind=Reset, pos=scene.caption_anchor, text='Reset')
+        ball = sphere(pos=vector(0, h0, 0), radius=1, color=color.magenta, make_trail=True, trail_color=color.white,
+                      trail_radius=0.1)
+        ground = box(pos=vector(0, 0, 0), length=100, height=0.01, width=100, opacity=0.5, color=color.orange)
+        time = 0
+        dt = 0.05
+        while time <= tc:
+            sleep(0.05)
+            ball.pos.y = h0 - v0 * np.sin((360 - theta) * np.pi / 180) * time - 0.5 * a * time ** 2
+            time = time + dt
+    plik = open('Rzuty.txt', 'w')
+    plik.write('Czas trwania ruchu wynosi: ' + str(tc) + '.\n')
+    if theta!=270:
+        plik.write('Zasięg ruchu: ' + str(z) + '.\n')
+    plik.write('Wysokosc maksymalna: ' + str(H) + '.\n')
+    plik.write('Prędkosc końcowa: ' + str(vk) + '.\n')
+    plik.close()
 def rzut2(h0,v0,a,theta):
     H=h0+((v0**2)*(np.sin(theta*np.pi/180)**2))/(2*a)
     tc=(v0*np.sin(theta*np.pi/180)/a)+np.sqrt(2*H/a)
@@ -107,6 +163,31 @@ def rzut2(h0,v0,a,theta):
         plt.grid(True)
         plt.savefig('WYKRES.jpg')
         plt.show
+
+        def Reset(q):
+            ball.make_trail = False
+            ball.clear_trail()
+            time = 0
+            ball.pos = vector(0, h0, 0)
+            ball.make_trail = True
+            while time <= tc:
+                sleep(0.05)
+                ball.pos.x = vx * time
+                ball.pos.y = h0 + v0 * np.sin(theta * np.pi / 180) * time - 0.5 * a * time ** 2
+                time = time + dt
+
+        scene = canvas(title='Symulacja rzutu', autoscale=True)
+        button(bind=Reset, pos=scene.caption_anchor, text='Reset')
+        ball = sphere(pos=vector(0, h0, 0), radius=1, color=color.magenta, make_trail=True, trail_color=color.white,
+                      trail_radius=0.1)
+        ground = box(pos=vector(0, 0, 0), length=3 * z, height=0.01, width=100, opacity=0.5, color=color.orange)
+        time = 0
+        dt = 0.05
+        while time <= tc:
+            sleep(0.05)
+            ball.pos.x = vx * time
+            ball.pos.y = h0 + v0 * np.sin(theta * np.pi / 180) * time - 0.5 * a * time ** 2
+            time = time + dt
     else:
         fig, ax = plt.subplots(2, 1)
         fig.tight_layout(h_pad=4)
@@ -128,6 +209,36 @@ def rzut2(h0,v0,a,theta):
         plt.grid(True)
         plt.savefig('WYKRES.jpg')
         plt.show()
+
+        def Reset(q):
+            ball.make_trail = False
+            ball.clear_trail()
+            time = 0
+            ball.pos = vector(0, h0, 0)
+            ball.make_trail = True
+            while time <= tc:
+                sleep(0.05)
+                ball.pos.y = h0 + v0 * np.sin(theta * np.pi / 180) * time - 0.5 * a * time ** 2
+                time = time + dt
+
+        scene = canvas(title='Symulacja rzutu', autoscale=True)
+        button(bind=Reset, pos=scene.caption_anchor, text='Reset')
+        ball = sphere(pos=vector(0, h0, 0), radius=1, color=color.magenta, make_trail=True, trail_color=color.white,
+                      trail_radius=0.1)
+        ground = box(pos=vector(0, 0, 0), length=100, height=0.01, width=100, opacity=0.5, color=color.orange)
+        time = 0
+        dt = 0.05
+        while time <= tc:
+            sleep(0.05)
+            ball.pos.y = h0 + v0 * np.sin(theta * np.pi / 180) * time - 0.5 * a * time ** 2
+            time = time + dt
+    plik = open('Rzuty.txt', 'w')
+    plik.write('Czas trwania ruchu wynosi: ' + str(tc) + '.\n')
+    if theta != 90 and v0!=0:
+        plik.write('Zasięg ruchu: ' + str(z) + '.\n')
+    plik.write('Wysokosc maksymalna: ' + str(H) + '.\n')
+    plik.write('Prędkosc końcowa: ' + str(vk) + '.\n')
+    plik.close()
 try:
     h0=float(input('Z jakiej wysokości wyrzucono ciało? '))
     while h0<0:
@@ -162,3 +273,6 @@ if v0==0 or (theta>=0 and theta<=90):
     rzut2(h0,v0,a,theta)
 elif theta>=270 and theta<360:
     rzut1(h0,v0,a,theta)
+plik=open('Rzuty.txt','r')
+print(plik.read())
+plik.close()
